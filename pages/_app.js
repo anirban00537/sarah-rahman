817412c7a14b849798651283bf2d72e378058e2b
Anirban Roy <anirban00537@gmail.com>
2021-06-15 15:40:40 +0600

    BASE FILE

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { useRouter } from "next/router";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  var pathUrl = router.pathname.split("/");
+  var status = pathUrl.includes("admin");
+  console.log(status);
+  return status ? (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  ) : (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
